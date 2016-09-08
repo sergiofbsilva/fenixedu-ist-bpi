83c3adef4632e39eaad2c923249272c16f564149
David Martinho <davidmartinho@gmail.com>
2016-09-08 12:47:42 +0100

    Integrated actual registration declaration file; Added converter to tiff service

--- src/main/java/pt/ist/fenixedu/bpi/webservice/BPISyncWebService.java
+++ src/main/java/pt/ist/fenixedu/bpi/webservice/BPISyncWebService.java
@@ -1,14 +1,25 @@
 package pt.ist.fenixedu.bpi.webservice;
 
+import java.io.IOException;
 import java.util.Locale;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 
 import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.academic.domain.ExecutionYear;
 import org.fenixedu.academic.domain.Person;
 import org.fenixedu.academic.domain.person.Gender;
+import org.fenixedu.academic.domain.student.Student;
+import org.fenixedu.bennu.BPIIntegrationConfiguration;
+import org.fenixedu.bennu.core.rest.JsonBodyReaderWriter;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.media.multipart.file.StreamDataBodyPart;
 import org.joda.time.Period;
 import org.joda.time.YearMonthDay;
 
@@ -16,28 +27,37 @@ import com.qubit.solution.fenixedu.bennu.webservices.domain.webservice.WebServic
 import com.qubit.solution.fenixedu.bennu.webservices.domain.webservice.WebServiceServerConfiguration;
 import com.qubit.solution.fenixedu.bennu.webservices.services.server.BennuWebService;
 
+import pt.ist.registration.process.domain.RegistrationDeclarationFile;
+
 @WebService
 public class BPISyncWebService extends BennuWebService {
 
     private static final String DATE_FORMAT = "yyyyMMdd";
+    private final Client client;
+
+    public BPISyncWebService() {
+        this.client = ClientBuilder.newClient();
+        this.client.register(MultiPartFeature.class);
+        this.client.register(JsonBodyReaderWriter.class);
+    }
 
     @WebMethod
-    public BPISyncBean getUser(String fiscalCode) throws BPISyncException{
+    public BPISyncBean getUser(String fiscalCode) throws BPISyncException {
         Person person = (Person) Person.readByContributorNumber(fiscalCode);
 
-        if(person == null){
+        if (person == null) {
             throw new BPISyncException("User not found");
         }
 
-        String gender = person.getGender() == Gender.MALE? "M": "F";
+        String gender = person.getGender() == Gender.MALE ? "M" : "F";
 
         Period age = new Period(person.getDateOfBirthYearMonthDay(), new YearMonthDay());
 
-        if (age.getYears() < 18){
+        if (age.getYears() < 18) {
             throw new BPISyncException("User is underage");
         }
 
-        if(person.getUser().getBpiCard() == null || !person.getUser().getBpiCard().getAllowSendDetails()){
+        if (person.getUser().getBpiCard() == null || !person.getUser().getBpiCard().getAllowSendDetails()) {
             throw new BPISyncException("User does not allow to see details");
         }
 
@@ -60,12 +80,12 @@ public class BPISyncWebService extends BennuWebService {
         bean.setBorough(person.getParishOfResidence());
 
         String postalCode = person.getDefaultPhysicalAddress().getAreaCode();
-        if (postalCode.contains("-")){
+        if (postalCode.contains("-")) {
 
             String[] codes = postalCode.split("-");
             bean.setZipCode(codes[0]);
             bean.setStreetLayoutCode(codes[1]);
-        }else{
+        } else {
             bean.setZipCode(postalCode);
             bean.setStreetLayoutCode("");
         }
@@ -73,9 +93,25 @@ public class BPISyncWebService extends BennuWebService {
         bean.setDegreeType(degree.getDegreeType().getName().getContent(new Locale("pt-PT")));
         bean.setDegree(degree.getFilteredName(ExecutionYear.readCurrentExecutionYear(), new Locale("pt-PT")));
         bean.setId(person.getUsername());
-        bean.setEnrolmentAgreement(new byte[0]);
+        bean.setEnrolmentAgreement(getRegistrationDeclaration(person));
 
         return bean;
+    }
+
+    private byte[] getRegistrationDeclaration(Person person) {
+        Student student = person.getStudent();
+        RegistrationDeclarationFile registrationDeclarationFile = student.getLastRegistration().getRegistrationDeclarationFile();
+        final StreamDataBodyPart streamDataBodyPart = new StreamDataBodyPart("file",
+                registrationDeclarationFile.getStream(), registrationDeclarationFile.getFilename(), new MediaType("application", "pdf"));
+        try (final FormDataMultiPart formDataMultiPart = new FormDataMultiPart()) {
+            formDataMultiPart.bodyPart(streamDataBodyPart);
+            return this.client.target(BPIIntegrationConfiguration.getConfiguration().converterUrl())
+                    .path("api").path("v1").path("convert").request()
+                    .header("Authorization", BPIIntegrationConfiguration.getConfiguration().converterSecret())
+                    .post(Entity.entity(formDataMultiPart, MediaType.MULTIPART_FORM_DATA_TYPE),  byte[].class);
+        } catch (IOException e) {
+            return new byte[0];
+        }
     }
 
     public static boolean validate(final String username, final String password) {
